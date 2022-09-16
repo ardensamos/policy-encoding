b45bf5835164fa63cbe6a5cc8142607fb5bdacc7
ArdenS <arden.song@samos.insure>
2022-09-16 13:24:57 -0400

    Revert

--- scripts/decoder.js
+++ scripts/decoder.js
@@ -19,8 +19,6 @@ function base35_to_int(x) {
 
 function id_decoder() {
   const x = document.getElementById("policy-number").value;
-  x = x.replace(/0/g, "O");
-
   const type_bit = 4;
   const partner_bit = 7;
   const region_bit = 8;
